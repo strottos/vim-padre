13976a13e231a2ab798d4ad91d737ae55875e7b1
Steven Trotter <stevetrot@gmail.com>
2019-06-09 09:27:16 +0100

    .

--- test/perf/steptest.py
+++ test/perf/steptest.py
@@ -5,6 +5,7 @@ Test stepping performance of Padre with Locust performance tester
 import re
 import select
 import time
+import uuid
 from datetime import datetime, timedelta
 from socket import socket
 
@@ -25,6 +26,7 @@ class PadreConnection():
         self.host = host
         self.socket = socket()
         self.counter = 1
+        self.thread_uuid = uuid.uuid1()
 
     def setup(self):
         """
@@ -35,7 +37,7 @@ class PadreConnection():
         port = int(socket_data[1]) if len(socket_data) > 1 else 12345
         self.socket.connect((host, port))
         self.socket.setblocking(0)
-        print("Setup Successfully")
+        print("{} Setup Successfully".format(self.thread_uuid))
 
     def step(self):
         """
@@ -45,29 +47,55 @@ class PadreConnection():
         try:
             counter = self._send("""{"cmd":"stepOver"}""")
             time_start = datetime.now()
-            data = None
+            data = ""
+            status_ok = False
+            position_ok = False
             while True:
-                if datetime.now() - time_start >= timedelta(TIMEOUT):
+                if datetime.now() - time_start >= timedelta(seconds=TIMEOUT):
+                    print("{} Timed out".format(self.thread_uuid))
                     raise TimeoutException
 
-                timeout = (time_start + timedelta(TIMEOUT) - datetime.now()) \
-                    / timedelta(seconds=1)
+                timeout = (time_start + timedelta(seconds=TIMEOUT)
+                           - datetime.now()) / timedelta(seconds=1)
+                print("{} Reading with timeout {}".format(self.thread_uuid,
+                                                          timeout))
                 ready = select.select([self.socket], [], [], timeout)
 
                 if ready[0]:
-                    data = self.socket.recv(4096).decode()
+                    data += self.socket.recv(4096).decode()
 
+                print("{} Checking right response {}".format(self.thread_uuid,
+                                                             data))
                 if re.match(""".*\\[{},{{"status":"OK"}}\\]"""
                             .format(counter), data):
+                    print("{} matched status".format(self.thread_uuid))
+                    status_ok = True
+
+                print("{} HERE".format(self.thread_uuid))
+
+                if re.match('.*\\["call",".*JumpToPosition",\\[[^\\]]*\\]\\]',
+                            data):
+                    print("{} matched position".format(self.thread_uuid))
+                    position_ok = True
+
+                print("{} HERE2".format(self.thread_uuid))
+
+                if status_ok and position_ok:
+                    data = ""
                     break
+
+                print("{} HERE3".format(self.thread_uuid))
+
         except Exception as e:
             total_time = int((time.time() - start_time) * 1000)
+            print("{} BAD {} {}".format(self.thread_uuid, total_time, e))
             events.request_failure.fire(request_type="execute",
                                         name="Step",
                                         response_time=total_time,
                                         exception=e)
         else:
             total_time = int((time.time() - start_time) * 1000)
+            print("{} GOOD {}".format(self.thread_uuid, total_time))
             events.request_success.fire(request_type="execute",
                                         name="Step",
                                         response_time=total_time,
