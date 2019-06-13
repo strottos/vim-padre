657b9a548f3fad1831bc789b5c7c44cc6f6e857d
Steven Trotter <stevetrot@gmail.com>
2019-06-13 08:34:11 +0100

    .

--- padre/src/debugger/node/debugger.rs
+++ padre/src/debugger/node/debugger.rs
@@ -9,9 +9,10 @@ use crate::debugger::Debugger;
 use crate::notifier::Notifier;
 use crate::util;
 
-use hyper::Client;
+use regex::Regex;
 use tokio::prelude::*;
 use tokio_process::CommandExt;
+use websocket::ClientBuilder;
 
 #[derive(Debug)]
 pub struct ImplDebugger {
@@ -77,35 +78,36 @@ impl Debugger for ImplDebugger {
         let reader = io::BufReader::new(node_stderr);
         let lines = tokio::io::lines(reader);
         tokio::spawn(
-            lines.for_each(move |l| {
-                eprintln!("{}", l);
+            lines.for_each(move |line| {
+                lazy_static! {
+                    static ref RE_NODE_STARTED: Regex =
+                        Regex::new("^Debugger listening on ws://127.0.0.1:\\d+/(.*)$").unwrap();
+                }
+
+                for cap in RE_NODE_STARTED.captures_iter(&line) {
+                    let node_debugger_hex = cap[1].to_string();
+                    println!("Node hex: {}", node_debugger_hex);
+                    let uri = format!("ws://localhost:{}/{}", port, node_debugger_hex);
+                    tokio::spawn(
+                        ClientBuilder::new(&uri)
+                            .unwrap()
+                            .add_protocol("rust-websocket")
+                            .async_connect_insecure()
+                            .and_then(|(duplex, _)| {
+                                let (sink, stream) = duplex.split();
+                                stream .forward(sink)
+                            })
+                    );
+                }
+
+                eprintln!("{}", line);
+
                 Ok(())
             })
             .map(|a| println!("stderr: {:?}", a))
             .map_err(|e| println!("stderr err: {:?}", e))
         );
 
-        tokio::spawn(
-            cmd.map(|a| {
-                println!("process: {}", a);
-            })
-            .map_err(|e| {
-                eprintln!("Error spawning node: {}", e);
-            }),
-        );
-
-        let uri = format!("http://127.0.0.1:{}/json", port).parse().unwrap();
-
-        tokio::spawn(
-            Client::new()
-                .get(uri)
-                .map(|out| {
-                    println!("Out: {:?}", out);
-                })
-                .map_err(|e| {
-                    println!("Error sending to node: {}", e);
-                })
-        );
         //let node_details: Vec<NodeDetails> = response.json().unwrap();
 
         let f = future::lazy(move || {
@@ -169,17 +171,6 @@ impl Debugger for ImplDebugger {
 
         Box::new(f)
     }
-}
-
-#[derive(Deserialize, Debug)]
-struct NodeDetails {
-    description: String,
-    devtoolsFrontendUrl: String,
-    faviconUrl: String,
-    id: String,
-    title: String,
-    url: String,
-    webSocketDebuggerUrl: String,
 }
 
 #[cfg(test)]
